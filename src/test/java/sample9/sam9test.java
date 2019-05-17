38671813125688b21d23cf6bb6dd16bdd6888115
vamshi <vamshi.doc001@gmail.com>
2019-05-17 14:01:00 +0530

    this is sample 9

---
+++ src/test/java/sample9/sam9test.java
@@ -0,0 +1,19 @@
+package sample9;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class sam9test {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
